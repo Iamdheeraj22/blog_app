b47abace3fade5fb52b1b26fa8401d8eef5a1c63
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-01-06 12:17:48 +0530

    feat: create new model for saving user & blogs data in hive storage.

---
+++ lib/core/common/entity/user_hive_box.dart
@@ -0,0 +1,7 @@
+import 'package:hive/hive.dart';
+
+class UserHiveBox {
+  final Box<dynamic> box;
+
+  UserHiveBox(this.box);
+}
---
+++ lib/features/blog/data/model/blog_hive_box_model.dart
@@ -0,0 +1,7 @@
+import 'package:hive/hive.dart';
+
+class BlogHiveBoxModel {
+  final Box<dynamic> box;
+
+  BlogHiveBoxModel(this.box);
+}
