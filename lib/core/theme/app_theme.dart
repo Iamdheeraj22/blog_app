2cd54ee16f8077bae1c8261ffc8b4ecf7427ef91
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-01-06 21:29:49 +0530

    feat : add light theme in app

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -21,6 +21,15 @@ class AppTheme {
       backgroundColor: AppPallete.backgroundColor,
       elevation: 0,
       surfaceTintColor: AppPallete.backgroundColor,
+      iconTheme: IconThemeData(
+        color: AppPallete.whiteColor,
+        size: 24,
+      ),
+      titleTextStyle: TextStyle(
+        color: AppPallete.whiteColor,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
     ),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: EdgeInsets.all(27),
@@ -29,5 +38,70 @@ class AppTheme {
       errorBorder: _border(AppPallete.redColor),
       focusedErrorBorder: _border(AppPallete.redColor),
     ),
+    textTheme: textTheme(color: AppPallete.whiteColor),
+  );
+  static final lightThemMode = ThemeData.dark().copyWith(
+    chipTheme: ChipThemeData(
+      color: WidgetStatePropertyAll(
+        AppPallete.blackColor,
+      ),
+      side: BorderSide.none,
+    ),
+    scaffoldBackgroundColor: AppPallete.whiteColor,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppPallete.whiteColor,
+      elevation: 0,
+      surfaceTintColor: AppPallete.whiteColor,
+      titleTextStyle: TextStyle(
+        color: AppPallete.blackColor,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      iconTheme: IconThemeData(
+        color: AppPallete.blackColor,
+        size: 24,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+      errorBorder: _border(AppPallete.redColor),
+      focusedErrorBorder: _border(AppPallete.redColor),
+    ),
+    textTheme: textTheme(color: AppPallete.blackColor),
   );
 }
+
+TextTheme textTheme({required Color color}) => TextTheme(
+      displayLarge:
+          TextStyle(fontSize: 57, fontWeight: FontWeight.bold, color: color),
+      displayMedium:
+          TextStyle(fontSize: 45, fontWeight: FontWeight.bold, color: color),
+      displaySmall:
+          TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: color),
+      headlineLarge:
+          TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: color),
+      headlineMedium:
+          TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: color),
+      headlineSmall:
+          TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: color),
+      titleLarge:
+          TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: color),
+      titleMedium:
+          TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: color),
+      titleSmall:
+          TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: color),
+      bodyLarge:
+          TextStyle(fontSize: 16, fontWeight: FontWeight.normal, color: color),
+      bodyMedium:
+          TextStyle(fontSize: 14, fontWeight: FontWeight.normal, color: color),
+      bodySmall:
+          TextStyle(fontSize: 12, fontWeight: FontWeight.normal, color: color),
+      labelLarge:
+          TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: color),
+      labelMedium:
+          TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: color),
+      labelSmall:
+          TextStyle(fontSize: 11, fontWeight: FontWeight.bold, color: color),
+    );
